70a77e768ad3fcc449d75aef3ddeb5288b2f296d
IlNevioIl <nevio.vogt@gmail.com>
2020-06-10 12:37:08 +0200

    hi

--- src/pack1/Var.java
+++ src/pack1/Var.java
@@ -14,6 +14,7 @@ public class Var {
 
     public int x = 50, y = 200, width = 95, height = 30;
     public static JButton jb1 = new yesbutton();
+    public static JButton jb2 = new question1.yesbutton();
 
 
     public static int x2 = 250;
@@ -23,8 +24,8 @@ public class Var {
     public static int x4 = 65, y2 = 0, width2 = 300, height2 = 200, size = 40;
     public static JLabel jl1 = new firstlabel();
 
-    public static int x5 = 65, y3 = 0, width3 = 300, height3 = 200, size2 = 40;
-    public static JLabel jl2 = new question();
+    //public static int x5 = 65, y3 = 0, width3 = 300, height3 = 200, size2 = 40;
+    //public static JLabel jl2 = new question();
 
 
 
